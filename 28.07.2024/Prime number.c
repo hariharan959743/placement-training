61a290bb2b967d8907feeabe608e617fea7c30d1
HARI HARAN <151361445+hariharan959743@users.noreply.github.com>
2024-07-29 08:55:33 +0530

    Create Prime number.c

---
+++ 28.07.2024/Prime number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    
+    if (isPrime(number)) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}

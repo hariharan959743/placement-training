6c284429b5c4997100c30a1198711298cbca222d
HARI HARAN <151361445+hariharan959743@users.noreply.github.com>
2024-08-01 13:31:09 +0530

    Create Decision making.py

---
+++ 1.08.2024 /Decision making.py
@@ -0,0 +1,18 @@
+
+age = 18
+if age < 18:
+    print("You are a minor.")
+elif age == 18:
+    print("You just turned 18.")
+else:
+    print("You are an adult.")
+temperature = 75
+if temperature > 85:
+    print("It's hot outside.")
+elif temperature > 65:
+    if temperature > 75:
+        print("It's warm outside.")
+    else:
+        print("It's pleasant outside.")
+else:
+    print("It's cold outside.")
